184688e26590a2191599796e138f477d369974f3
sataped <k.satapat@kkumail.com>
2020-08-30 17:07:50 +0700

    HW-Lec-97

---
+++ Lec-97/DataAccess.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_97
+{
+    class DataAccess
+    {
+        //Initialize the SQLite database
+        public  static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (userID INTEGER PRIMARY KEY, " +
+                    "firstName NVARCHAR(20) NOT NULL, "+
+                    "lastName NVARCHAR(20) NOT NULL, " +
+                    "EMail NVARCHAR(20) NOT NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        //Insert
+        public static void AddData(string inputuserID, string inputfirstName, string inputlastName, string inputemail)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (@userID, @firstName, @lastName, @email);";
+                insertCommand.Parameters.AddWithValue("@userID", inputuserID);
+                insertCommand.Parameters.AddWithValue("@firstName", inputfirstName);
+                insertCommand.Parameters.AddWithValue("@lastName", inputlastName);
+                insertCommand.Parameters.AddWithValue("@email", inputemail);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        //Retrieve data from the SQLite database
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT userID, firstName, lastName, email from Customers", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
---
+++ Lec-97/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lec_97
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(userIDtxt.Text, firstNametxt.Text, lastNametxt.Text, emailtxt.Text);
+            userIDtxt.Clear();
+            firstNametxt.Clear();
+            lastNametxt.Clear();
+            emailtxt.Clear();
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string Showdata = "";
+            foreach (string i in DataAccess.GetData())
+            {
+                Showdata = Showdata + i + "\n";
+            }
+            MessageBox.Show(Showdata);
+        }
+    }
+}
